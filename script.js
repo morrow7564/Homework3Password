bed1e9ecd760a7e6c2b85efa5c629aff1aa3afc9
morrow7564 <morrow7564@gmail.com>
2020-09-15 21:05:35 -0400

    adding final code for homework3 password

--- script.js
+++ script.js
@@ -1,19 +1,85 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var taken in from HTML page
+var resultEl = document.getElementById('result');
+var lengthEl = document.getElementById('length');
+var uppercaseEl = document.getElementById('uppercase');
+var lowercaseEl = document.getElementById('lowercase');
+var numbersEl = document.getElementById('numbers');
+var symbolsEl = document.getElementById('symbols');
+var generateEl = document.getElementById('generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// var that adds that adds the function together from letters, numbers and symbols
+var randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
 
-  passwordText.value = password;
+// functions for the Letters, Numbers and symbols
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  return String.fromCharCode(Math.floor(Math.random() * 11) + 33)
 }
 
 
+// click listner function
+generate.addEventListener('click', () => {
+	var length = +lengthEl.value;
+	var hasLower = lowercaseEl.checked;
+	var hasUpper = uppercaseEl.checked;
+	var hasNumber = numbersEl.checked;
+	var hasSymbol = symbolsEl.checked;
+	
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
 
+// function for whats actully going to happen when you hit click
+function generatePassword(lower, upper, number, symbol, length) {
+	var generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	//if nothing is checked then nothing will happen 
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// a loop so the the password can generate each type 
+	for(var i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	var finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+clipboard.addEventListener('click', () => {
+	const textarea = document.createElement('textarea');
+	const password = resultEl.innerText;
+	
+	if(!password) { return; }
+	
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+});
 
 
